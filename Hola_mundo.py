bb38599378f1e9b119d925f3beff2e6a06d56884
Jonathan <jamezquita150@gmail.com>
2024-02-20 18:31:05 -0600

    comentarios

--- Hola_mundo.py
+++ Hola_mundo.py
@@ -1,2 +1,6 @@
 print("Hola mundo")
-print("Hola","mi nonbre es","Jonathan")
+print("Hola","mi nombre es","Jonathan")
+print("No tengo ni la mas minima idea de lo que estoy haciendo\npero voy a seguir intentando")
+#los caracteres \n generan un salto de linea.
+#estos siempre deben estar dentro del texto con comillas.
+#si esta fuera lo tomara como algo invalido.
