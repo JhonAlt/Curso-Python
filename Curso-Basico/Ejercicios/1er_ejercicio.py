bdbc4c455e7782d1f2fdd444b39e9b024430be26
Jonathan Amézquita <jamezquita150@gmail.com>
2025-04-12 22:51:54 -0600

    Actualización del archivo 1er_ejercicio.py con nuevos ejercicios y comentarios explicativos sobre el uso de print(), tipos de datos y concatenación de cadenas.

--- Curso-Basico/Ejercicios/1er_ejercicio.py
+++ Curso-Basico/Ejercicios/1er_ejercicio.py
@@ -1,13 +1,18 @@
 
+#1. Imprime "¡Hola Mundo!" por consola.
 print("Hola mundo, como estas?")
+#2. Escribe un comentario de una sola línea explicando qué hace el código del Ejercicio 1.
 #Utiliza la función print para imprimir un mensaje en la consola.
 
+#3. Imprime tu nombre y edad en la misma línea utilizando la función print.
 print("Mi nombre es Jonathan y tengo 27 años")
 
+#4.  Usa la función type() para imprimir el tipo de dato de una cadena de texto, un número entero y un número decimal.
 print(type("Esto es una cadena de texto"))
 print(type(1247))
 print(type(1.1416))
 
+#5. Escribe un comentario en varias líneas explicando qué son los tipos de datos en Python. 
 '''
 En Python existen diferentes tipos de datos:
 los de tipo string (str(Hola soy un texto)) 
@@ -17,15 +22,21 @@ los de tipo booleanos (bool(true o false))
 los de tipo complejos (complex(3j + 5j))
 
 '''
+
+#6. Imprime el resultado de concatenar dos cadenas de texto, por ejemplo: "Hola" y "Mundo".
 print("Hola " + "Mundo")
 
+#7. Crea una variable para almacenar tu nombre, otra para tu edad, e imprime ambas en la misma línea.
 name = "Jonathan"
 age = 27
 print(f"Mi nombre es {name} y tengo {age} años")
 
+#8. Escribe un programa que solicite al usuario su nombre y lo imprima junto con un saludo.
 user_name = input("¿Cuál es tu nombre? ")
 print(f"Mucho gusto {user_name}, bienvenido")
 
+#9. Usa print() para mostrar el resultado de la suma de dos números enteros y el tipo de dato resultante.
 num1 = 50 + 85 #Creamos una variable con la suma de dos números enteros
 print(num1) #Se imprime en consola el resultado
 print(type(num1)) #Se imprime el tipo de dato del resultado
+#10. Comenta el código del Ejercicio 9, y explica qué hace cada línea usando comentarios de una sola línea.
