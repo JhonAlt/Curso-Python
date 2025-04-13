44fcc0cf823009b796dabea0aa529496dcd53c7e
Jonathan Amézquita <jamezquita150@gmail.com>
2025-04-12 22:44:04 -0600

    Agregados operadores de asignación, comparación, lógicos e identidad en el archivo de operaciones aritméticas.

--- Curso-Basico/03_op_aritmeticos.py
+++ Curso-Basico/03_op_aritmeticos.py
@@ -31,3 +31,54 @@ print("Hola " * (2**3))#Esto ya es solo por probar XD
 my_float = 2.5 * 2 #Esto da como resultado 5.0
 #print("Hola " * my_float)#Esto da un error porque no se puede concatenar un float a un string
 print("Hola " * int(my_float))#Esto convierte el float a un entero y lo concatena a un string
+
+#operadores de asignación
+my_int = 0
+my_int += 5 #my_int = my_int + 5
+print(my_int)
+
+my_int -= 2 #my_int = my_int - 2
+print(my_int)
+
+my_int *= 2 #my_int = my_int * 2
+print(my_int)
+
+my_int /= 2 #my_int = my_int / 2
+print(my_int)
+
+my_int %= 2 #my_int = my_int % 2
+print(my_int)
+
+my_int **= 2 #my_int = my_int ** 2
+print(my_int)
+
+my_int //= 2 #my_int = my_int // 2
+print(my_int)
+
+#operadores de comparación
+my_int = 10
+my_int2 = 20
+
+print(my_int == my_int2)#igualdad
+print(my_int != my_int2)#diferencia
+
+print(my_int > my_int2)#mayor que
+print(my_int < my_int2)#menor que
+print(my_int >= my_int2)#mayor o igual que
+print(my_int <= my_int2)#menor o igual que
+
+#operadores lógicos
+my_bool = True
+my_bool2 = False
+
+print(my_bool and my_bool2)#and
+print(my_bool or my_bool2)#or
+print(not my_bool)#not
+
+#operadores de identidad
+my_int = 10
+my_int2 = 10
+
+print(my_int is my_int2)#is
+print(my_int is not my_int2)#is not
+
---
+++ Curso-Basico/Ejercicios/1er_ejercicio.py
@@ -0,0 +1,31 @@
+
+print("Hola mundo, como estas?")
+#Utiliza la función print para imprimir un mensaje en la consola.
+
+print("Mi nombre es Jonathan y tengo 27 años")
+
+print(type("Esto es una cadena de texto"))
+print(type(1247))
+print(type(1.1416))
+
+'''
+En Python existen diferentes tipos de datos:
+los de tipo string (str(Hola soy un texto)) 
+los de tipo enteros (int(122456))
+los de tipo decimales (float(1.2548))
+los de tipo booleanos (bool(true o false))
+los de tipo complejos (complex(3j + 5j))
+
+'''
+print("Hola " + "Mundo")
+
+name = "Jonathan"
+age = 27
+print(f"Mi nombre es {name} y tengo {age} años")
+
+user_name = input("¿Cuál es tu nombre? ")
+print(f"Mucho gusto {user_name}, bienvenido")
+
+num1 = 50 + 85 #Creamos una variable con la suma de dos números enteros
+print(num1) #Se imprime en consola el resultado
+print(type(num1)) #Se imprime el tipo de dato del resultado
